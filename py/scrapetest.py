2af840d5bb747cd924a08763b7cdb97f8d3aae62
dain-lee <dain.lee@navercorp.com>
2017-05-30 14:06:58 +0900

    getHtml / getTitle 메소드 정의 예외처리 추가

--- py/scrapetest.py
+++ py/scrapetest.py
@@ -1,6 +1,25 @@
 from urllib2 import urlopen
+from urllib2 import HTTPError
 from bs4 import BeautifulSoup
 
-html = urlopen("http://pythonscraping.com/pages/page1.html")
-bsObj = BeautifulSoup(html.read(), "html.parser")
-print(bsObj.h1)
+def getHtml(url):
+    try:
+        return urlopen(url)
+    except HTTPError as e:
+        print("failed to open url")
+        return None
+
+def getTitle(html):
+    # what if html is none?? -> AttributeError
+    try:
+        bsObj = BeautifulSoup(html.read(), "html.parser")
+        return bsObj.body.h1
+    except AttributeError as e:
+        return None
+
+html = getHtml("http://pythonscraping.com/pages/page1.html")
+title = getTitle(html)
+if (title == None):
+    print("Title is not found")
+else:
+    print(title)
